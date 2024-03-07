202c7aa684dffe9f38caf8a0bfdbbec1450e8f8a
nischay-18 <chnischaysai@gmail.com>
2024-03-07 11:57:58 -0500

    commit wb

--- src/PortfolioContainer/Home/Profile/Profile.js
+++ src/PortfolioContainer/Home/Profile/Profile.js
@@ -1,74 +1,68 @@
-import React from "react";
-import Typewriter from "typewriter-effect";
-import "./Profile.css";
-import pdf from "../../../assets/Resume.pdf"
-import { FaGithub, FaLinkedin, FaEnvelope, FaPhone, FaMapMarkerAlt } from "react-icons/fa";
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+import './Profile.css';
+import pdf from '../../../assets/Resume.pdf';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 export default function Profile() {
   return (
     <div className="profile-container">
       <div className="profile-parent">
         <div className="profile-details">
-          
           <div className="profile-details-name">
             <span className="primary-text">
-              {" "}
               Hello, I'm <span className="highlighted-text">Nischay Sai Cherukuri</span>
             </span>
           </div>
           <div className="profile-details-role">
             <span className="primary-text">
-              {" "}
               <h1>
-               
                 <Typewriter
-                onInit={(typewriter) => {
-                  typewriter
-                    .typeString("Data Enthusiast")
-                    .pauseFor(200)
-                    .deleteAll()
-                    .typeString("Machine Learning")
-                    .pauseFor(200)
-                    .deleteAll()
-                    .typeString("Deep Learning")
-                    .pauseFor(200)
-                    .deleteAll()
-                    .typeString("Natural Language Processing")
-                    .pauseFor(200)
-                    .deleteAll()
-                    .typeString("Cloud Computing Enthusiast")
-                    .pauseFor(200)
-                    .deleteAll()
-                    .typeString("Big Data Analyst")
-                    .pauseFor(200)
-                    .start();
-                }}
-                options={{
-                  autoStart: true,
-                  loop: true,
-                }}
-              />
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString("Data Enthusiast 📊")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("Machine Learning 🤖")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("Deep Learning 🧠")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("Natural Language Processing 📝")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("Cloud Computing Enthusiast ☁️")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("Big Data Analyst 💾")
+                      .pauseFor(200)
+                      .start();
+                  }}
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
               </h1>
             </span>
             <span className="profile-role-tagline">
-            Empowering Decisions with Data-Driven Solutions.
+              Empowering Decisions with Data-Driven Solutions.
             </span>
           </div>
 
           <div className="profile-options">
-            <div>
-            <a  href={pdf} target="_blank" rel="noopener noreferrer">
+            <a href={pdf} target="_blank" rel="noopener noreferrer">
               <button className="btn highlighted-btn">Get Resume</button>
             </a>
+            <div className="profile-social">
+              <a className="color-white" href="https://github.com/nischay-18" target="_blank" rel="noopener noreferrer">
+                <FaGithub size={24} />
+              </a>
+              <a href="https://www.linkedin.com/in/nischaysaicherukuri/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin size={24} />
+              </a>
             </div>
-            
-            <div class="profie-social">
-                        <a class="color-white" href="https://github.com/nischay-18" target="_blank" rel="noopener noreferrer">
-                            <FaGithub size={24} />
-                        </a>
-                        <a href="https://www.linkedin.com/in/nischaysaicherukuri/" target="_blank" rel="noopener noreferrer">
-                            <FaLinkedin size={24} />
-                        </a>
-                    </div>
           </div>
         </div>
         <div className="profile-picture">
--- src/PortfolioContainer/workexperience/workexperience.js
+++ src/PortfolioContainer/workexperience/workexperience.js
@@ -38,6 +38,22 @@ export default function WorkExperience(props) {
       </div>
       <div className="work-experience-container">
     
+      <div className="job-details">
+        <div className="header">
+            <h6 className="heading">The World Bank Group</h6>
+            <span className="date">January 2024 - Present</span>
+        </div>
+        <p className="position">Data Science Consultant</p>
+     <div className="hi">
+        <ul>
+            <li>Assessed and visualized EU rail/road traffic flows using Eurostat data in NUTS 2 regions.</li>
+            <li>Developed an interactive dashboard for country-level EU rail and road transport data analysis.</li>
+            <li>Applied Machine Learning and econometrics for predicting railway stock prices and demand elasticity, aiding transport sector strategy.</li>
+            <li>Collaborated in a team to propose emission reduction solutions aligned with the EU Green Deal, emphasizing transportation sector innovation and tech adoption.</li>
+        </ul>
+        </div>
+    </div>
+
     <div className="job-details">
         <div className="header">
             <h6 className="heading">The George Washington University, USA</h6>
@@ -56,15 +72,14 @@ export default function WorkExperience(props) {
 
     <div className="job-details">
         <div className="header">
-            <h6 className="heading">UberGrad</h6>
+            <h6 className="heading">GTCUBE</h6>
             <span className="date">May 2021 – September 2021</span>
         </div>
         <p className="position">Data Science Intern</p>
         <ul>
-            <li>Developed and optimized machine learning models for categorizing universities, enhancing student application recommendations.</li>
-            <li>Utilized data and predictive analytics to discern sales trends and anticipate future course demand.</li>
+            <li>Developed and optimized machine learning models for categorizing universities, enhancing student application recommendations, and improving accuracy by 20%.</li>
+            <li>Leveraged predictive analytics for sales trend insights, boosting sales by 25% and guiding course and pricing strategy.</li>
             <li>Reduced data processing times by 15% by migrating workflows to AWS and optimizing resources.</li>
-            <li>Enhanced the U-SURE PRO engine by integrating 20+ profile parameters, ensuring tailored university recommendations for users.</li>
         </ul>
     </div>
 
