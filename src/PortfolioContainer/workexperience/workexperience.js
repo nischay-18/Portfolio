15c37111a86ea516b4f50022a5b598b6083a5962
nischay-18 <chnischaysai@gmail.com>
2025-07-14 10:11:50 -0400

    resume and work experience

--- src/PortfolioContainer/workexperience/workexperience.js
+++ src/PortfolioContainer/workexperience/workexperience.js
@@ -37,21 +37,24 @@ export default function WorkExperience(props) {
         <div className="animation-container" ref={animationContainer}></div>
       </div>
       <div className="work-experience-container">
-    
+      <div className="job-details">
+        <div className="header">
+            <h6 className="heading">DC Water</h6>
+            <span className="date">October 2024 - Present</span>
+        </div>
+        <div className="job-details">
+        <div className="header">
+            <h6 className="heading">GW, EEMI</h6>
+            <span className="date">July 2024 - June 2025</span>
+        </div>
+        <p className="position">Data Consultant</p>
+    </div>
       <div className="job-details">
         <div className="header">
             <h6 className="heading">The World Bank Group</h6>
             <span className="date">January 2024 - May 2024</span>
         </div>
         <p className="position">Data Science Consultant</p>
-     <div className="hi">
-        <ul>
-            <li>Assessed and visualized EU rail/road traffic flows using Eurostat data in NUTS 2 regions.</li>
-            <li>Developed an interactive dashboard for country-level EU rail and road transport data analysis.</li>
-            <li>Applied Machine Learning and econometrics for predicting railway stock prices and demand elasticity, aiding transport sector strategy.</li>
-            <li>Collaborated in a team to propose emission reduction solutions aligned with the EU Green Deal, emphasizing transportation sector innovation and tech adoption.</li>
-        </ul>
-        </div>
     </div>
 
     <div className="job-details">
@@ -60,31 +63,11 @@ export default function WorkExperience(props) {
             <span className="date">March 2023 - May 2024 </span>
         </div>
         <p className="position">Web Analyst</p>
-     <div className="hi">
-        <ul>
-            <li>Managed and optimized content across GW Engineering and departmental websites using HTML and Drupal.</li>
-            <li>Implemented SEO strategies, achieving a 7% increase in overall website score and enhancing key metrics.</li>
-            <li>Analyzed website data to inform content strategy and align with user behavior trends.</li>
-            <li>Collaborated with the communications director and academic staff to ensure cohesive communication strategies.</li>
-        </ul>
-        </div>
-    </div>
-
-    <div className="job-details">
-        <div className="header">
-            <h6 className="heading">GT CUBE</h6>
-            <span className="date">May 2021 – July 2022</span>
-        </div>
-        <p className="position">Data Scientist</p>
-        <ul>
-            <li>Developed and optimized machine learning models for categorizing universities, enhancing student application recommendations, and improving accuracy by 20%.</li>
-            <li>Leveraged predictive analytics for sales trend insights, boosting sales by 25% and guiding course and pricing strategy.</li>
-            <li>Reduced data processing times by 15% by migrating workflows to AWS and optimizing resources.</li>
-        </ul>
     </div>
 
 </div>
       </div>
+    </div>
     </div>
    
   );
